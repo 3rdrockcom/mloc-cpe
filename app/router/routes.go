11ab782ab8f87a3145bebf22af69cb0a64b0477c
Frank <fvaland@fintechadvance.com>
2018-05-07 16:46:42 -0700

    Changed routes to have group prefix.

--- app/router/routes.go
+++ app/router/routes.go
@@ -3,7 +3,12 @@ package router
 func (r *Router) appendRoutes() {
 	r.e.GET("/ping", r.c.Ping)
 
-	r.e.POST("/customers/add", r.c.PostAddCustomer)
-	r.e.POST("/customers/:customerID/transactions/add", r.c.PostAddCustomerTransactions)
-	r.e.GET("/customers/:customerID/profile", r.c.GetCustomerProfile)
+	api := r.e.Group("/api")
+	v1 := api.Group("/v1")
+
+	v1.GET("/ping", r.c.Ping)
+
+	v1.POST("/customers/add", r.c.PostAddCustomer)
+	v1.POST("/customers/:customerID/transactions/add", r.c.PostAddCustomerTransactions)
+	v1.GET("/customers/:customerID/profile", r.c.GetCustomerProfile)
 }
