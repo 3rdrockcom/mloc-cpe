b7afb415281444df1072a217bbeee66d1935665b
Frank <fvaland@fintechadvance.com>
2018-05-16 17:58:42 -0700

    Moved related endpoints to same controller file.

--- app/controllers/auth.go
+++ app/controllers/auth.go
@@ -9,6 +9,26 @@ import (
 	"github.com/labstack/echo"
 )
 
+func (co Controllers) GetCustomerKey(c echo.Context) error {
+	programID := 1
+
+	programCustomerID, err := strconv.Atoi(c.QueryParam("customer_id"))
+	if err != nil {
+		return SendErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	programCustomerMobile := c.QueryParam("mobile")
+
+	api := API.New()
+	customerAccessKey, err := api.GetCustomerAccessKey(programID, programCustomerID, programCustomerMobile)
+	if err != nil {
+		return SendErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	return SendResponse(c, http.StatusOK, customerAccessKey)
+
+}
+
 func (co Controllers) GenerateCustomerKey(c echo.Context) error {
 	programID := 1
 
--- app/controllers/customers.go
+++ app/controllers/customers.go
@@ -2,9 +2,14 @@ package controllers
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/epointpayment/customerprofilingengine-demo-classifier-api/app/models"
 	Customer "github.com/epointpayment/customerprofilingengine-demo-classifier-api/app/services/customer"
+	"github.com/epointpayment/customerprofilingengine-demo-classifier-api/app/services/profiler"
 
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/jinzhu/now"
 	"github.com/labstack/echo"
 )
 
@@ -34,4 +39,71 @@ func (co Controllers) PostAddCustomer(c echo.Context) error {
 	}
 
 	return SendResponse(c, http.StatusOK, customer)
+}
+
+type payloadTransactions struct {
+	Transactions []models.Transaction `json:"transactions" binding:"required"`
+}
+
+func (co Controllers) PostAddCustomerTransactions(c echo.Context) error {
+	customerID := c.Get("customerID").(int)
+
+	payload := payloadTransactions{}
+	if err := c.Bind(&payload); err != nil {
+		return SendErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	transactions := payload.Transactions
+	for i := range transactions {
+		transactions[i].CustomerID = customerID
+		transactions[i].DateTime = transactions[i].Date.Time
+	}
+
+	if err := validation.Validate(transactions); err != nil {
+		return SendErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	sc, err := Customer.New(customerID)
+	if err != nil {
+		return err
+	}
+
+	if err = sc.Transactions().Create(transactions); err != nil {
+		return SendErrorResponse(c, http.StatusInternalServerError, err.Error())
+	}
+
+	return SendResponse(c, http.StatusOK, transactions)
+}
+
+func (co Controllers) GetCustomerProfile(c echo.Context) error {
+	customerID := c.Get("customerID").(int)
+
+	startDate, err := time.ParseInLocation(
+		"20060102",
+		c.QueryParam("startDate"), time.UTC)
+	if err != nil {
+		return SendErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	endDate, err := time.ParseInLocation(
+		"20060102",
+		c.QueryParam("endDate"), time.UTC)
+	if err != nil {
+		return SendErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	sc, err := Customer.New(customerID)
+	if err != nil {
+		return err
+	}
+
+	transactions := models.Transactions{}
+	if transactions, err = sc.Transactions().GetAllByDateRange(startDate, now.New(endDate).EndOfDay()); err != nil {
+		return SendErrorResponse(c, http.StatusInternalServerError, err.Error())
+	}
+
+	p := profiler.New(transactions, 2)
+	res := p.Run()
+
+	return SendResponse(c, http.StatusOK, res)
 }
